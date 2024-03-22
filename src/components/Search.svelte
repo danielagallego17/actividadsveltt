5c96de7f91ac3b17310797257783d01c8087dfa4
Gxrman-exe <germanacosta1808@gmail.com>
2024-03-22 01:12:07 -0500

    Proyecto Notes Svelte

---
+++ src/components/Search.svelte
@@ -0,0 +1,16 @@
+<script>
+    let query= '';
+</script>
+
+<div class="Search">
+    <div class="Search-container">
+        <input type="text" placeholder="Buscar" bind:value={query} on:input>
+    </div>
+</div>
+
+<style>
+    
+    input{
+        width: 500px;
+    }
+</style>
---
+++ src/components/Switch.svelte
@@ -0,0 +1,51 @@
+<script>
+
+    import { darkmode } from '../store/store.js';
+
+	function changeTheme() {
+        darkmode.update(x => !$darkmode);
+
+        fetch("http://localhost:3001/darkmode", { 
+			method: "POST",
+			body: JSON.stringify({darkmode: $darkmode}), 
+			headers: { 
+				"Content-type": "application/json; charset=UTF-8"
+			} 
+		}).then(response => response.json())
+		.then(res => console.log(res));
+	}
+</script>
+
+<div class="Switch">
+    <div class={ ($darkmode? 'switch-container on': 'switch-container')}>
+       <button class="circle" on:click={changeTheme} ></button>
+    </div>
+    
+</div>
+
+
+<style>
+    .switch-container{
+        background-color: #ccc;
+        width: 50px;
+        height: 25px;
+        border-radius: 30px;
+        margin: 0 10px;
+    }
+
+    .switch-container .circle{
+        cursor: pointer;
+        width: 25px;
+        height: 25px;
+        border-radius: 50%;
+        transition: all 0.2s;
+    }
+
+    .on .circle{
+        margin-left: 25px;
+    }
+
+    .on{
+        background-color: blue;
+    }
+</style>
